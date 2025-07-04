3617a8f722c8f8e3e79e872787397672d1729493
Misha Molchan <mishamolchan@Mikkes-MacBook-Pro.local>
2025-07-04 23:02:31 +0300

    version 0.2 beta

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, DateField
 from wtforms.validators import DataRequired
 
 from flask_sqlalchemy import SQLAlchemy
@@ -25,12 +25,13 @@ db.init_app(app)
 
 
 
-class Employee(db.Model):
+class Employee1(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True,  autoincrement=True)
     name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
     position: Mapped[str] = mapped_column(String(250), unique=False, nullable=False)
     birthday: Mapped[str] = mapped_column(String(250), unique=False, nullable=False)
-    vacation: Mapped[str] = mapped_column(String(250), unique=False, nullable=False)
+    vacation_start: Mapped[str] = mapped_column(String(250), unique=False, nullable=False)
+    vacation_end: Mapped[str] = mapped_column(String(250), unique=False, nullable=False)
     email: Mapped[str] = mapped_column(String(250), unique=False, nullable=False)
     contract: Mapped[str] = mapped_column(String(250), unique=False, nullable=False)
  
@@ -42,16 +43,16 @@ class Employee(db.Model):
 
 class AddEmployeeForm(FlaskForm):
     name = StringField('Имя', validators=[DataRequired()])
-    position = StringField('Должность', validators=[DataRequired()])
-    birthday = StringField('Дата рождения', validators=[DataRequired()])
-    vacation = StringField('Отпуск', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired()])
-    contract = StringField('Истечение контракта', validators=[DataRequired()])
+    position = StringField('Должность')
+    birthday = StringField('Дата рождения')
+    vacation_start = StringField('Начало отпуска')
+    vacation_end = StringField('Конец отпуска')
+    email = StringField('Email')
+    contract = StringField('Истечение контракта')
     submit = SubmitField('Добавить')
 
 
 
-    
 
 
 
@@ -60,7 +61,16 @@ def home():
     form = AddEmployeeForm()
     if form.validate_on_submit():
         if request.method == 'POST':
-            add_employee(form.name.data, form.position.data, form.birthday.data, form.vacation.data, form.email.data, form.contract.data)
+            if not check_if_exists(form.name.data):
+                add_employee(form.name.data, form.position.data, form.birthday.data, form.vacation_start.data, form.vacation_end.data, form.email.data, form.contract.data)
+            else:
+                if form.vacation_start.data and form.vacation_start.data:
+                    update_vacation_by_name(form.name.data, form.vacation_start.data, form.vacation_end.data)
+                elif form.contract.data:
+                    update_contract_by_name(form.name.data, form.contract.data)
+                else:
+                    pass
+
             return render_template('index.html', form=form, employees=get_employees())
     return render_template('index.html', form=form, employees=get_employees())
 
@@ -69,23 +79,47 @@ def home():
 
 
 
-def add_employee(name: str, position: str, birthday: str, vacation: str = 'None', email: str = 'None', contract: str = 'None') -> None:
+
+
+def add_employee(name: str, position: str = 'None', birthday: str = 'None', vacation_start: str = 'None', vacation_end: str = 'None', email: str = 'None', contract: str = 'None') -> None:
     with app.app_context():
-        employee = Employee(name=name, position=position, birthday=birthday, vacation=vacation, email=email, contract=contract)
+        employee = Employee1(name=name, position=position, birthday=birthday, vacation_start=vacation_start, vacation_end=vacation_end, email=email, contract=contract)
         db.session.add(employee)
         db.session.commit()
 
 
 def get_employees() -> tuple[object]:
     with app.app_context():
-        employees = Employee.query.all()
+        employees = Employee1.query.all()
         # db.session.commit()
     return employees
 
+def check_if_exists(name: str) -> bool:
+    employees = get_employees()
+    for employee in employees:
+        if name == employee.name:
+            return True
+        else:
+            continue
+    return False
+
+def update_vacation_by_name(name: str, vacation_start: str, vacation_end: str) -> None:
+    with app.app_context():
+        employee = Employee1.query.filter_by(name=name).first()
+        if employee:
+            employee.vacation_start = vacation_start
+            employee.vacation_end = vacation_end
+            db.session.commit()
+
+def update_contract_by_name(name: str, contract: str) -> None:
+    with app.app_context():
+        employee = Employee1.query.filter_by(name=name).first()
+        if employee:
+            employee.contract = contract
+            db.session.commit()
     
 
-
-
+print(check_if_exists)
 
 
 if __name__ == '__main__':
